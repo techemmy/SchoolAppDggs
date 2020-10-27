aa06ffd460053d2b0941e7c510f2337c37fbac79
Emmanuel <emmanueltopea@gmail.com>
2020-10-27 02:36:58 +0100

    'update'

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
-from app import app
+from app import app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
